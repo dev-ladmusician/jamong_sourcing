83d9ca94ce4aef1627066f923682633ce09b3522
sagong2 <janghan3150@gmail.com>
2016-04-27 17:02:21 +0900

    중복 코드 제거/ 클래스 정리(contact)

--- application/views/_HOME/contact.php
+++ application/views/_HOME/contact.php
@@ -1,15 +1,20 @@
-<section class="jm-contact">
-    <div class="content-wrapper col-lg-9 col-md-9 col-sm-12 solid-border-box">
-        <div class="header">CONTACT</div>
-        <iframe id="jm-map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3234.306562559269!2d128.56473903951846!3d35.84148577537712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3565e49d95856907%3A0xa27d257eca7f24ff!2z64yA6rWs6rSR7Jet7IucIOuCqOq1rCDrjIDrqoXrj5kgMTYyOS0xMA!5e0!3m2!1sko!2skr!4v1461053845618"
-            frameborder="0" allowfullscreen></iframe>
-        <div class="content">
-            <p><span>E-MAIL</span> help@vr-jamong.com</p>
+<div class="content-wrapper col-lg-9 col-md-9 col-sm-12">
+    <section class="jm-contact">
+        <div class="row solid-border-empty">
+            <div class="page-header solid-border-bottom col-lg-12">
+                CONTACT</div>
 
-            <p><span>ADDRESS</span> 대구광역시 남구 대명동 1629-9 동우빌딩 2층</p>
+            <div class="content col-lg-12">
+                <iframe id="jm-map"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3234.306562559269!2d128.56473903951846!3d35.84148577537712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3565e49d95856907%3A0xa27d257eca7f24ff!2z64yA6rWs6rSR7Jet7IucIOuCqOq1rCDrjIDrqoXrj5kgMTYyOS0xMA!5e0!3m2!1sko!2skr!4v1461053845618"
+                        frameborder="0" allowfullscreen></iframe>
 
-            <p><span>TEL</span> 053-654-2015</p>
+                <p><span>E-MAIL</span> help@vr-jamong.com</p>
+
+                <p><span>ADDRESS</span> 대구광역시 남구 대명동 1629-9 동우빌딩 2층</p>
+
+                <p><span>TEL</span> 053-654-2015</p>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+</div>
