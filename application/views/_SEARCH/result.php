b53ca06e835f3183df6da6b09cec3d583106cd0f
sagong2 <janghan3150@gmail.com>
2016-04-27 16:06:47 +0900

    중복 코드 제거/클래스 정리 (search, main)

--- application/views/_SEARCH/result.php
+++ application/views/_SEARCH/result.php
@@ -1,32 +1,30 @@
 <div class="content-wrapper col-lg-9 col-md-9 col-sm-12">
     <section class="jm-search">
+        <div class="row solid-border-empty">
+            <div class="header solid-border-bottom"><?= '"' . $search_query . '" ' . $count ?>개의 VR컨텐츠
+                <select title="jm-filter">
+                    <optgroup label="업로드 날짜">
+                        <option value="">오늘</option>
+                        <option value="">1일 전</option>
+                        <option value="">일주일 전</option>
+                        <option value="">한달 전</option>
+                        <option value="">1년 전</option>
+                    </optgroup>
+                    <optgroup label="정렬 기준">
+                        <option value="">조회수</option>
+                        <option value="">좋아요</option>
+                        <option value="">업로드 날짜</option>
+                    </optgroup>
+                    <optgroup label="기능별">
+                        <option value="">3D 360</option>
+                        <option value="">사진</option>
+                        <option value="">2D</option>
+                    </optgroup>
+                </select>
+            </div>
 
-        <div class="header solid-border-bottom"><?= '"' . $search_query . '" ' . $count ?>개의 VR컨텐츠
-            <select class=" solid-border-box float-right" title="필터">
-                <optgroup label="업로드 날짜">
-                    <option value="">오늘</option>
-                    <option value="">1일 전</option>
-                    <option value="">일주일 전</option>
-                    <option value="">한달 전</option>
-                    <option value="">1년 전</option>
-                </optgroup>
-                <optgroup label="정렬 기준">
-                    <option value="">조회수</option>
-                    <option value="">좋아요</option>
-                    <option value="">업로드 날짜</option>
-                </optgroup>
-                <optgroup label="기능별">
-                    <option value="">3D 360</option>
-                    <option value="">사진</option>
-                    <option value="">2D</option>
-                </optgroup>
-            </select>
-        </div>
-
-
-        <div class="category-list">
-            <div class="col-lg-12 col-md-12 col-sm-12 padding-outer">
-                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6 padding-inner">
+            <div class="result-list col-lg-12 col-md-12 col-sm-12 padding-outer solid-border-bottom">
+                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6">
                     <div class="solid-border-box">
                         <img src="/JAMONG/static/img/ex_1.png" alt="">
 
@@ -39,7 +37,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6 padding-inner">
+                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6">
                     <div class="solid-border-box">
                         <img src="/JAMONG/static/img/ex_2.png" alt="">
 
@@ -52,7 +50,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6 padding-inner">
+                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6">
                     <div class="solid-border-box">
                         <img src="/JAMONG/static/img/ex_3.png" alt="">
 
@@ -65,7 +63,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6 padding-inner">
+                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6">
                     <div class="solid-border-box">
                         <img src="/JAMONG/static/img/ex_4.png" alt="">
 
@@ -79,7 +77,7 @@
                     </div>
 
                 </div>
-                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6 padding-inner">
+                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6">
                     <div class="solid-border-box">
                         <img src="/JAMONG/static/img/ex_5.png" alt="">
 
@@ -92,7 +90,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6 padding-inner">
+                <div class="video-small col-lg-3 col-md-3 col-sm-4 col-xs-6">
                     <div class="solid-border-box">
                         <img src="/JAMONG/static/img/ex_6.png" alt="">
 
@@ -107,12 +105,11 @@
                 </div>
 
             </div>
+
+            <div class="col-lg-12 col-md-12 col-sm-12 load-more padding-normal text-center">
+                <a href="#">더보기<i class="glyphicon glyphicon-menu-down"></i></a>
+            </div>
+
         </div>
-
-        <div class="solid-border-box col-lg-12 col-md-12 col-sm-12 padding-normal text-center">
-            <a class="load-more" href="#">더보기<i class="glyphicon glyphicon-menu-down"></i></a>
-        </div>
-
-
     </section>
 </div>
