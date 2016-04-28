7cbad6f2858757eb9ca05a90bab89ab761fd999a
sagong2 <janghan3150@gmail.com>
2016-04-28 12:30:32 +0900

    VR 재생화면 (Player) - 왼쪽 하단 댓글 입력 form , 댓글 리스트 아이템

--- application/views/_PLAYER/index.php
+++ application/views/_PLAYER/index.php
@@ -1,100 +1,97 @@
 <div class="content-wrapper col-lg-9 col-md-9 col-sm-12">
     <section class="jm-player">
-        <div class=" col-lg-9 col-md-8 padding-none">
-            <div class="jm-player-video solid-border-box ">
-<!--                <div class="video-large ">-->
-<!--                    <img src="--><?//= site_url('/static/img/video_1.jpg') ?><!--" alt="">-->
-<!--                </div>-->
-                <div id="pano"></div>
-                <div class="video-des padding-normal solid-border-bottom">
-                    <p class="video-des-title">Fit girls - 커플요가</p>
+        <div class="row">
+            <div class=" col-lg-9 col-md-8 padding-none">
 
-                    <div class="video-info">
-                        <div class="video-des-publisher">게시자</div>
-                        <div class="video-des-date">게시일</div>
-                        <div class="video-des-hits">조회수</div>
+                <div class="jm-player-video solid-border-box col-lg-12">
+                    <div id="pano"></div>
+                    <div class="video-des padding-normal solid-border-bottom">
+                        <p class="video-des-title">Fit girls - 커플요가</p>
+
+                        <div class="video-info">
+                            <div class="video-des-publisher">게시자</div>
+                            <div class="video-des-date">게시일</div>
+                            <div class="video-des-hits">조회수</div>
+                        </div>
+                    </div>
+
+                    <div class="video-action padding-normal">
+                        <a href=#"><i class="glyphicon glyphicon-heart-empty"></i>11111</a>
+                        <a href="#"><i class="glyphicon glyphicon-share-alt"></i>공유</a>
                     </div>
                 </div>
-                <div class="video-action padding-normal">
-                    <a href=#"><i class="glyphicon glyphicon-heart-empty"></i>11111</a>
-                    <a href="#"><i class="glyphicon glyphicon-share-alt"></i>공유</a>
+
+                <div class="jm-player-comment solid-border-box col-lg-12">
+                    <div class="header solid-border-bottom">
+                        댓글 <span>70</span>
+                    </div>
+                    <div id="jm-form-comment" class="solid-border-bottom ">
+                        <div class="comment-img">
+                            <img src="<?= site_url('/static/img/user.png') ?>" alt="">
+                        </div>
+                        <div class="comment-content">
+                            <textarea name="user-comment" id="jm-comment" cols="54" rows="4"></textarea>
+                        </div>
+                        <div class="btn-cm-submit comment-submit">
+                            <a href="#" class="">등록</a>
+                        </div>
+                    </div>
+                    <div class="comment-list-container">
+                        <ul class="comment-list">
+                            <li class="comment-group">
+                                <div class="comment-item-img">
+                                    <img src="<?= site_url('/static/img/user.png') ?>" alt="">
+                                </div>
+                                <div class="comment-item-contents">
+                                    <div class="comment-item-name">
+                                        <span>Jamong</span>
+                                        <span>신고</span>
+                                    </div>
+                                    <div class="comment-item-content">
+                                        테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다.
+                                        테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다.
+                                        테스트입니다.
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="load-more col-lg-12 col-md-12 col-sm-12 text-center">
+                    <a href="#">더보기<i class="glyphicon glyphicon-menu-down"></i></a>
                 </div>
             </div>
-            <div class="jm-player-comment solid-border-box">
-                <div class="header solid-border-bottom padding-normal">
-                    댓글 <span>70</span>
+
+            <div class="col-lg-3 col-md-4">
+                <div class="jm-player-channel solid-border-box col-lg-12 padding-none">
+                    <div class="image-container ">
+                        <img src="<?= site_url('static/img/ex_1.png') ?>" alt="">
+                    </div>
+
+
+                    <div class="des-group">
+                        <div class="des-title">
+                            Jamong
+                        </div>
+
+                        <div class="des-info">
+                            구독자 <span>2154명</span>
+                        </div>
+                        <a href="" class="btn-ch-subs"><i class="glyphicon glyphicon-plus"></i>구독하기</a>
+
+                    </div>
                 </div>
-                <div id="jm-form-comment" class="col-lg-12 col-md-12 col-sm-12 text-center solid-border-bottom padding-normal">
-                    <div class="col-lg-2 col-md-3 col-sm-3 padding-none">
-                        <img  src="<?= site_url('/static/img/user.png') ?>" alt="">
-                    </div>
-                    <div class="col-lg-8 col-md-6 col-sm-6 padding-none">
-                        <textarea  name="user-comment" id="jm-comment" cols="50" rows="3"></textarea>
-                    </div>
-                    <div class="col-lg-2 col-md-3 col-sm-3 btn-cm-submit ">
-                        <a href="#" class="">등록</a>
-                    </div>
-                </div>
-                <div class=" solid-border-box padding-normal col-lg-12 col-md-12 col-sm-12">
-                    <ul class="comment-list padding-none">
-                        <li class="comment-item padding-normal">
-                            <div>
-                                <img src="<?= site_url('/static/img/user.png') ?>" alt="">
-                            </div>
-                            <div>
-                                <span>Jamong</span>
 
-                                <div>테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다.</div>
-                            </div>
-                        </li>
 
-                        <li class="comment-item padding-normal">
-                            <div>
-                                <img src="<?= site_url('/static/img/user.png') ?>" alt="">
-                            </div>
-                            <div>
-                                <span>Jamong</span>
+                <div class="jm-player-recommend solid-border-box col-lg-12 ">
+                    <div class="header solid-border-bottom padding-normal">
+                        추천 VR컨텐츠
+                    </div>
+                    <ul>
 
-                                <div>테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다. 테스트입니다.</div>
-                            </div>
-                        </li>
                     </ul>
                 </div>
-
-            </div>
-            <div class="solid-border-box col-lg-12 col-md-12 col-sm-12 padding-normal text-center">
-                <a class="load-more" href="#">더보기<i class="glyphicon glyphicon-menu-down"></i></a>
-            </div>
-        </div>
-
-        <div class="col-lg-3 col-md-4">
-            <div class="jm-player-channel solid-border-box col-lg-12 padding-none">
-                <div class="image-container ">
-                    <img src="<?= site_url('static/img/ex_1.png') ?>" alt="">
-                </div>
-
-
-                <div class="des-group">
-                    <div class="des-title">
-                        Jamong
-                    </div>
-
-                    <div class="des-info">
-                        구독자 <span>2154명</span>
-                    </div>
-                    <a href="" class="btn-ch-subs"><i class="glyphicon glyphicon-plus"></i>구독하기</a>
-
-                </div>
-            </div>
-
-
-            <div class="jm-player-recommend solid-border-box col-lg-12 ">
-                <div class="header solid-border-bottom padding-normal">
-                    추천 VR컨텐츠
-                </div>
-                <ul>
-
-                </ul>
             </div>
         </div>
     </section>
