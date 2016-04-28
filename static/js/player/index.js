16dd37f2e08079fb79f294a8f43254f2236bfce8
sagong2 <janghan3150@gmail.com>
2016-04-28 16:38:27 +0900

    VR 재생 페이지(player) js로 더보기(댓글) 버튼과 높이 맞추기

--- static/js/player/index.js
+++ static/js/player/index.js
@@ -14,5 +14,27 @@
 //        // There will be an 'alert()' error when no target will be set.
 //        target: "pano"
 //    });
-var dir = 'static/js/krpano/';
-embedpano({swf:dir + "krpano.swf", xml:dir +"krpano.xml", target:"pano", html5:"only"});
+
+
+$(window).load(function(){
+    reset_recommend_list_max_height();
+});
+$(document).ready(function(){
+    var dir = 'static/js/krpano/';
+    embedpano({swf:dir + "krpano.swf", xml:dir +"krpano.xml", target:"pano", html5:"only"});
+
+    $('.load-more').click(function(){
+        reset_recommend_list_max_height();
+    })
+});
+
+function reset_recommend_list_max_height(){
+    var row_height = $('.content-wrapper .row div:first-child').outerHeight();
+    var upper_item_height = $('.jm-player-channel').outerHeight(true);
+    var target_header = $('.jm-player-recommend .header').outerHeight();
+    console.log("row" + row_height);
+    console.log("upper" + upper_item_height);
+    console.log("header" + target_header);
+    var target = $('.jm-player-recommend .recommend-list');
+    target.css("max-height", row_height - upper_item_height - target_header + "px");
+}
