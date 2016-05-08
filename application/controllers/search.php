3634f9dfc7dcb43f6ebabf39f4b2364fdcab7df9
sagong2 <janghan3150@gmail.com>
2016-05-09 08:23:32 +0900

    get results by search_query and count of the result

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -8,6 +8,7 @@ class Search extends CORE_Controller
         parent::__construct();
         $this->load->model('category_model');
         $this->load->model('channel_model');
+        $this->load->model('contents_model');
     }
 
     function index()
@@ -21,9 +22,12 @@ class Search extends CORE_Controller
 
         $categories= $this->category_model->gets();
         $channels = $this->channel_model->gets();
+        $result = $this->contents_model->get_items_by_search($str, 1);
 
-        $count = 6;
+        $count = $this->contents_model->get_total_count_by_search($str);
 
+        var_dump($result);
+        var_dump($count);
         $this->__get_views('_SEARCH/result', array('categories' => $categories, 'channels'=>$channels),
             array('search_query' => $str,
                 'count' => $count));
--- application/views/_AUTH/find_password.php
+++ application/views/_AUTH/find_password.php
@@ -1,7 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: SangBeom
- * Date: 2016-05-07
- * Time: 오전 4:00
- */
+<div class="row">
+    <section class="jm-login">
+        <div class="container">
+
+            <div class="form-container solid-border-box">
+
+                <div class="header solid-border-bottom">
+                    <a href="<?= site_url('/auth/login') ?>">로그인</a>
+                    <a href="<?= site_url('/auth/register') ?>">회원가입</a>
+                </div>
+                <form id="jm-auth-find-password" action="post">
+                    <div class="form-group">
+                        비밀번호 찾기
+                    </div>
+                    <div class="form-group">
+                        <input type="email" placeholder="이메일" class="solid-border-box">
+                    </div>
+                    <div class="form-group btn-login-submit">
+                        <a href="">비밀번호 재설정</a>
+                    </div>
+                    <ul class="form-group">
+                        <li>가입시 등록했던 이메일로 임시 비밀번호를 보내드립니다.</li>
+                        <li>메일 발송 후 1시간 이내로 발송된 비밀번호로 로그인을 해야 변경됩니다.</li>
+                    </ul>
+                </form>
+            </div>
+        </div>
+    </section>
+</div>
