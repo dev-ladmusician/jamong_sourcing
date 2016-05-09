84c8c9c3d808226f523a66cd123ae801d7adf86f
sagong2 <janghan3150@gmail.com>
2016-05-09 18:47:51 +0900

    채널 구독하기 > 2. Player 우측 상단 channel_info / insert channel_profile_image

---
+++ application/controllers/api/player.php
@@ -0,0 +1,44 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Player extends CORE_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('subscriber_model');
+    }
+
+    function subscribe_update(){
+        $this->__require_login();
+
+        $contentId = $this->input->get('contentId');
+        $userId = $this->session->userdata('userid');
+        $channelId = $this->input->get('channelId');
+        $is_subscribed = $this->input->get('is_subscribed');
+
+        //구독할때 add
+        if(strcmp($is_subscribed, 'true') == 0){
+            $rtv = $this->subscriber_model->add($channelId,$userId);
+            if($rtv=='1'){
+                $this->session->set_flashdata('message', '채널을 구독했습니다.');
+                redirect('player?contentId='.$contentId);
+            }else{
+                $this->session->set_flashdata('message', '채널을 구독하는 데 오류가 발생했습니다.');
+                redirect('player?contentId='.$contentId);
+            }
+        }else{
+            $rtv = $this->subscriber_model->delete($channelId,$userId);
+            if($rtv=='1'){
+                $this->session->set_flashdata('message', '채널 구독을 취소했습니다.');
+                redirect('player?contentId='.$contentId);
+            }else{
+                $this->session->set_flashdata('message', '채널을 구독을 취소하는데 오류가 발생했습니다.');
+                redirect('player?contentId='.$contentId);
+            }
+        }
+
+
+    }
+
+}
