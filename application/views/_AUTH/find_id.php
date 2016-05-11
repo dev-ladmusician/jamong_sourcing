8b2c345dce1fa33b25a86ed6df458963d8a688eb
sagong2 <janghan3150@gmail.com>
2016-05-11 20:45:07 +0900

    회원가입/아이디찾기/비밀번호 찾기 : 양옆 스크롤 제거

--- application/views/_AUTH/find_id.php
+++ application/views/_AUTH/find_id.php
@@ -1,25 +1,23 @@
-<div class="row">
-    <section class="jm-login">
-        <div class="container">
+<section class="jm-login">
+    <div class="container">
 
-            <div class="form-container solid-border-box">
+        <div class="form-container solid-border-box">
 
-                <div class="header solid-border-bottom">
-                    <a href="<?= site_url('/auth/login') ?>">로그인</a>
-                    <a href="<?= site_url('/auth/register') ?>">회원가입</a>
-                </div>
-                <form id="jm-auth-find-id" action="<?= site_url('/api/auth/submit_find_id')?>" method="post">
-                    <div class="form-group">
-                        이메일 찾기
-                    </div>
-                    <div class="form-group">
-                        <input name="nickName" type="text" placeholder="닉네임" class="solid-border-box" required>
-                    </div>
-                    <div class="form-group">
-                        <input class="btn-login-submit" type="submit" value="이메일 찾기">
-                    </div>
-                </form>
+            <div class="header solid-border-bottom">
+                <a href="<?= site_url('/auth/login') ?>">로그인</a>
+                <a href="<?= site_url('/auth/register') ?>">회원가입</a>
             </div>
+            <form id="jm-auth-find-id" action="<?= site_url('/api/auth/submit_find_id') ?>" method="post">
+                <div class="form-group">
+                    이메일 찾기
+                </div>
+                <div class="form-group">
+                    <input name="nickName" type="text" placeholder="닉네임" class="solid-border-box" required>
+                </div>
+                <div class="form-group">
+                    <input class="btn-login-submit" type="submit" value="이메일 찾기">
+                </div>
+            </form>
         </div>
-    </section>
-</div>
+    </div>
+</section>
